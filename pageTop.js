3b84a7fb424f22c2abef39981c14c886169e4aad
yuma999 <79397514+yuma999@users.noreply.github.com>
2021-06-11 16:46:25 +0900

    Add files via upload

--- pageTop.js
+++ pageTop.js
@@ -18,4 +18,28 @@ $ (function(){
     $('body, html').animate({ scrollTop: 0}, 500);
     return false;
   })
+
+
+
+  var parallaxBkImg = function(){
+    $(window).on('load resize', function() {
+      $(window).on('load scroll', function(){
+        var $winTop = $(window).scrollTop();
+        var $target = $('.menu.top');
+        var $target = $('.about.top');
+        var $target = $('.location.top');
+        var $winWidth = $(window).width();
+        if($winWidth < 736) {
+          $target.each(function(index){
+            var $position = $winTop - $target.eq(index).offset().top;
+            if($winTop > $target.eq(index).offset().top - 800) {
+              $target.eq(index).css({
+                'background-position-y': $position * .4
+              });
+            }
+          });
+        }
+      });
+    });
+  }();
 })
